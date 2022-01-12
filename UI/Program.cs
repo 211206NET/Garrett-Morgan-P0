c26482f26889953e464770c3ed788a28ccc6ea03
gam0146 <95451035+gam0146@users.noreply.github.com>
2022-01-12 11:37:10 -0600

    Update Program.cs

--- UI/Program.cs
+++ UI/Program.cs
@@ -4,5 +4,7 @@ using UI;
 IRepo repo = new FileRepo();
 RRBL bl = new RRBL(repo);
 MainMenu menu = new MainMenu(bl);
-
+Log.Logger = new LoggerConfiguration()
+.WriteTo.File(@"..\DL\logger.txt")
+.CreateLogger();
 menu.Start();
